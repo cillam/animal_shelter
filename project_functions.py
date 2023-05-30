5b0706e43fb0e6cdd79a660d3af33c7dec683940
Priscilla Miller <cilla.m.miller@gmail.com>
2023-05-30 13:13:01 -0700

    Added plotting related to adoptions. Updated import statement for project_functions.py

--- project_functions.py
+++ project_functions.py
@@ -5,7 +5,6 @@ Created on Wed May 24 09:47:57 2023
 @author: Priscilla Miller
 """
 import matplotlib.pyplot as plt
-from numpy import arange
 
 MONTHS = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul",
           8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
@@ -18,7 +17,7 @@ def monthly_in_out(count, year, category):
     plt.style.use('seaborn-v0_8-colorblind')
     b = plt.bar(count.index.values, count, width=0.6)
     plt.bar_label(b)
-    plt.title(f"Number of {category} per Month ({year})", size = 20,
+    plt.title(f"Number of {category} per Month ({year})", size=20,
               weight="bold")
     plt.ylabel(f"{category}")
     plt.xlabel("Month")
@@ -47,37 +46,15 @@ def type_pie_chart(types, title):
               bbox_to_anchor=(1, 0, 0.5, 1))
 
     plt.setp(autotexts, size=12)
-    ax.set_title(f"{title}", size = 20, weight="bold")
+    ax.set_title(f"{title}", size=20, weight="bold")
 
     plt.show()
+    plt.close(plt.gcf())
 
-# this function will be updated to plot monthly average against adoption average
-def monthly_in_out_grouped(count1, count2, year):
-    months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
-              "Oct", "Nov", "Dec")
-    categories = {
-        "Intakes": count1,
-        "Outcomes": count2,
-        }
 
-    x = arange(len(months))
-    width = 0.4
-    multiplier = 0
+def convertAgeToYears(age):
+    return age/365.24
 
-    plt.figure(figsize=(12, 6))
-    plt.style.use('seaborn-v0_8-colorblind')
-    ax = plt.subplot()
 
-    for category, count in categories.items():
-        offset = width * multiplier
-        rects = ax.bar(x + offset, count, width, label=category)
-        ax.bar_label(rects, padding=3)
-        multiplier += 1
-
-    ax.set_ylabel("Intake/Outcome Count")
-    ax.set_title(f"Number of Intakes/Outcomes per Month ({year})")
-    ax.set_xticks(x + width, months)
-    ax.legend(ncols=3)
-
-    plt.show()
-
+def convertDaysInShelterToYears(days):
+    return days/365.24
